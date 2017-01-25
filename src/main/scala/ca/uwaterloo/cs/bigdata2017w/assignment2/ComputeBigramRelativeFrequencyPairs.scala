f4eab58ffe7983c3eb9caf07a95207c5b6868a05
yf3sun <yf3sun@student.cs.uwaterloo.ca>
2017-01-25 01:39:27 -0500

    blah

--- src/main/scala/ca/uwaterloo/cs/bigdata2017w/assignment2/ComputeBigramRelativeFrequencyPairs.scala
+++ src/main/scala/ca/uwaterloo/cs/bigdata2017w/assignment2/ComputeBigramRelativeFrequencyPairs.scala
@@ -1,38 +1,48 @@
-/**
-  * Bespin: reference implementations of "big data" algorithms
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  * http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+package ca.uwaterloo.cs.bigdata2016w.v2kovac.assignment2;
 
-package ca.uwaterloo.cs.bigdata2017w.assignment2
-
-import io.bespin.scala.util.Tokenizer
-
+import collection.mutable.HashMap
+import scala.collection.JavaConverters._
+import java.util.StringTokenizer
 import org.apache.log4j._
 import org.apache.hadoop.fs._
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
+import org.apache.spark.Partitioner
 import org.rogach.scallop._
+import org.apache.spark.util.{CollectionsUtils, Utils}
 
-class Conf(args: Seq[String]) extends ScallopConf(args) with Tokenizer {
+trait Tokenizer {
+  def tokenize(s: String): List[String] = {
+    new StringTokenizer(s).asScala.toList
+      .map(_.asInstanceOf[String].toLowerCase().replaceAll("(^[^a-z]+|[^a-z]+$)", ""))
+      .filter(_.length != 0)
+  }
+}
+
+class Conf(args: Seq[String]) extends ScallopConf(args) with Tokenizer  {
   mainOptions = Seq(input, output, reducers)
   val input = opt[String](descr = "input path", required = true)
   val output = opt[String](descr = "output path", required = true)
   val reducers = opt[Int](descr = "number of reducers", required = false, default = Some(1))
-  verify()
 }
 
-object BigramCount extends Tokenizer {
+// Copied HashPartition from spark docs, and changed getParition
+class HashPartitioner(partitions: Int) extends Partitioner {
+  def numPartitions: Int = partitions
+  def getPartition(key: Any): Int = key match {
+    case null => 0
+    case (k1,k2) => (k1.hashCode & Int.MaxValue) % numPartitions
+  }
+  override def equals(other: Any): Boolean = other match {
+    case h: HashPartitioner =>
+      h.numPartitions == numPartitions
+    case _ =>
+      false
+  }
+  override def hashCode: Int = numPartitions
+}
+
+object ComputeBigramRelativeFrequencyPairs extends Tokenizer {
   val log = Logger.getLogger(getClass().getName())
 
   def main(argv: Array[String]) {
@@ -42,20 +52,31 @@ object BigramCount extends Tokenizer {
     log.info("Output: " + args.output())
     log.info("Number of reducers: " + args.reducers())
 
-    val conf = new SparkConf().setAppName("Bigram Count")
+    val conf = new SparkConf().setAppName("Bigram Pairs")
     val sc = new SparkContext(conf)
 
     val outputDir = new Path(args.output())
     FileSystem.get(sc.hadoopConfiguration).delete(outputDir, true)
 
-    val textFile = sc.textFile(args.input())
-    val counts = textFile
+    var marginal = 0.0
+
+    val textFile = sc.textFile(args.input(), args.reducers())
+    textFile
       .flatMap(line => {
         val tokens = tokenize(line)
-        if (tokens.length > 1) tokens.sliding(2).map(p => p.mkString(" ")).toList else List()
+        if (tokens.length > 1) {
+          val bigrams = tokens.sliding(2).map(p => (p.head,p.tail.mkString)).toList
+          val individuals = tokens.init.map(w => (w,"*")).toList
+          bigrams ++ individuals
+        } else List()
       })
       .map(bigram => (bigram, 1))
       .reduceByKey(_ + _)
-    counts.saveAsTextFile(args.output())
+      .repartitionAndSortWithinPartitions(new HashPartitioner(args.reducers()))
+      .map(p => p._1 match {
+        case (_,"*") => {marginal = p._2; (p._1,p._2)}
+        case (_,_) => (p._1,p._2 / marginal)
+      })
+      .saveAsTextFile(args.output())
   }
 }
